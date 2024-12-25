0f2dbc72d149826e77d4f47d0e1d488d4c7c6d2f
Niels Van Goethem <nielsvangoethem@hotmail.com>
2024-12-25 21:26:40 +0100

    Add Inner glow box

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,29 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://neilvangotham.github.io",
+  vite: {
+    resolve: {
+      alias: {
+        "@/": `${path.resolve(__dirname, "src")}/`,
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "sass:math";
+            @use "@/styles/vars.scss" as *;
+          `,
+        },
+      },
+    },
+  },
 });
